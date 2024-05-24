c4cf64c89151f916f122fe67a214bd64df146c39
Nguyễn Công Thành <thanhncde160490@fpt.edu.vn>
2024-05-24 22:55:08 +0700

    update: Header and Sign-in, Sign-up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react';
 import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-import OAuth from '../components/Oauth';
+import OAuth from '../components/OAuth'
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
