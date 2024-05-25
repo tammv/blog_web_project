518e90d8716130cc9bdff7ae8e1e4d38fecddcd7
minhdev313 <voducminh313@gmail.com>
2024-05-25 16:01:46 +0700

    Demo PayOS
    -Run npm i xong run npm start

---
+++ api/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    isPremium:{
+        type: Boolean,
+        default: false
+    },
+    profilePicture:{
+        type: String,
+        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    dateOfPre:{
+        type: Date
+    }
+},{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
