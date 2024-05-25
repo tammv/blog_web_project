25f671b791db8e344c3c27c81751babb103d0ecf
minhdev313 <voducminh313@gmail.com>
2024-05-25 15:34:09 +0700

    Demo
    Đang lỗi google authen

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,36 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true
+    password: {
+      type: String,
+      required: true,
     },
-    isAdmin:{
-        type: Boolean,
-        default: false
+    profilePicture: {
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
-    isPremium:{
-        type: Boolean,
-        default: false
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
-    profilePicture:{
-        type: String,
-        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-    },
-    dateOfPre:{
-        type: Date
-    }
-},{timestamps: true});
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "devb-blog.firebaseapp.com",
-  projectId: "devb-blog",
-  storageBucket: "devb-blog.appspot.com",
-  messagingSenderId: "26828132532",
-  appId: "1:26828132532:web:644df8d0c5cb5aa7aac7e1"
+  authDomain: 'mern-blog-b327f.firebaseapp.com',
+  projectId: 'mern-blog-b327f',
+  storageBucket: 'mern-blog-b327f.appspot.com',
+  messagingSenderId: '699397991367',
+  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
 };
 
 // Initialize Firebase
