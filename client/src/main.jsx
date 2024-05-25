e7f517a3bbc5afeb9140c10260591e952eae6803
Tammv <72090950+tammv@users.noreply.github.com>
2024-05-25 16:29:13 +0700

    Complete dark mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store, persistor } from './redux/store.js'
-import {Provider} from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider"; // Import without curly braces
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <PersistGate persistor={persistor}>
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  </PersistGate>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
+);
