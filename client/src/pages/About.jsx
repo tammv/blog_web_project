aa18a9d9d38ad48609f1c5ff24ef93cbc9d90a33
Tammv <72090950+tammv@users.noreply.github.com>
2024-05-29 11:06:41 +0700

    update UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function About() {
+  const { theme } = useSelector((state) => state.theme);
   return (
-    <div>About</div>
-  )
+    <section className={`py-10 sm:py-16 lg:py-24 ${theme === "dark" ? "dark:bg-gray-900" : ""}`}>
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
+          <div>
+            <h1
+              className={`text-4xl font-bold sm:text-6xl lg:text-7xl ${theme === "dark" ? "text-white" : "text-black"}`}
+            >
+              Welcome to DEVB – Your Gateway to the World of Technology
+              {/* <div className="relative inline-flex">
+                <span className="absolute inset-x-0 bottom-0 border-b-[30px] border-[#4ADE80]"></span>
+                <h1
+                  className={`relative text-4xl font-bold sm:text-6xl lg:text-7xl ${
+                    theme === "dark" ? "text-white" : "text-black"
+                  }`}
+                >
+                  Postcrafts.
+                </h1>
+              </div> */}
+            </h1>
+
+            <p className={`mt-8 text-base sm:text-xl ${theme === "dark" ? "text-gray-300" : "text-black"}`}>
+              Our mission is to create a platform where technology enthusiasts can connect, collaborate, and share their
+              knowledge. We aim to foster a community where learning is continuous, and innovation is at the forefront.
+            </p>
+
+            <div className="mt-10 sm:flex sm:items-center sm:space-x-8">
+              <a
+                href="#"
+                title=""
+                className={`inline-flex items-center justify-center px-10 py-4 text-base font-semibold transition-all duration-200 ${
+                  theme === "dark"
+                    ? "text-white bg-orange-600 hover:bg-orange-700 focus:bg-orange-700"
+                    : "text-white bg-orange-500 hover:bg-orange-600 focus:bg-orange-600"
+                }`}
+                role="button"
+              >
+                {" "}
+                Start exploring{" "}
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <img
+              className="w-full"
+              src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/2/hero-img.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,278 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <section className="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24 dark:bg-gray-900">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
+            <div>
+              <p className="text-base font-semibold tracking-wider text-blue-600 uppercase dark:text-blue-400">
+                A Tech Blog for Knowledge Sharing
+              </p>
+              <h1 className="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl dark:text-white">
+                Connect & Learn from the Experts
+              </h1>
+              <p className="mt-4 text-base text-black lg:mt-8 sm:text-xl dark:text-gray-300">
+                Accelerate your career growth with insights from industry leaders. Join a community where technology
+                enthusiasts share their knowledge, experiences, and innovations.
+              </p>
+              <a
+                href="/sign-up"
+                title=""
+                className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:bg-yellow-600"
+                role="button"
+              >
+                Join for free
+                <svg
+                  className="w-6 h-6 ml-8 -mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </a>
+              <p className="mt-5 text-gray-600 dark:text-gray-400">
+                Already joined us?{" "}
+                <a
+                  href="/sign-in"
+                  title=""
+                  className="text-black transition-all duration-200 hover:underline dark:text-white"
+                >
+                  Log in
+                </a>
+              </p>
+            </div>
+            <div>
+              <img
+                className="w-full"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/1/hero-img.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-10 bg-gray-50 sm:py-16 lg:py-24 dark:bg-gray-800">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="flex items-end justify-between">
+            <div className="flex-1 text-center lg:text-left">
+              <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl dark:text-white">
+                Latest from blog
+              </h2>
+              {/* <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600 lg:mx-0 dark:text-gray-400">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.
+              </p> */}
+            </div>
+            <div className="hidden lg:flex lg:items-center lg:space-x-3">
+              <button
+                type="button"
+                className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white dark:border-gray-600 dark:hover:bg-blue-500 dark:focus:bg-blue-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+                </svg>
+              </button>
+              <button
+                type="button"
+                className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white dark:border-gray-600 dark:hover:bg-blue-500 dark:focus:bg-blue-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="grid max-w-md grid-cols-1 gap-6 mx-auto mt-8 lg:mt-16 lg:grid-cols-3 lg:max-w-full">
+            <div className="overflow-hidden bg-white rounded shadow dark:bg-gray-700">
+              <div className="p-5">
+                <div className="relative">
+                  <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                    <img
+                      className="object-cover w-full h-full"
+                      src="https://cdn.rareblocks.xyz/collection/celebration/images/blog/2/blog-post-1.jpg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="absolute top-4 left-4">
+                    <span className="px-4 py-2 text-xs font-semibold tracking-widest text-gray-900 uppercase bg-white rounded-full dark:text-gray-100 dark:bg-gray-800">
+                      Lifestyle
+                    </span>
+                  </div>
+                </div>
+                <span className="block mt-6 text-sm font-semibold tracking-widest text-gray-500 uppercase dark:text-gray-400">
+                  March 21, 2020
+                </span>
+                <p className="mt-5 text-2xl font-semibold">
+                  <a href="#" title="" className="text-black dark:text-white">
+                    How to build coffee inside your home in 5 minutes.
+                  </a>
+                </p>
+                <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
+                  enim velit mollit.
+                </p>
+                <a
+                  href="#"
+                  title=""
+                  className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600 dark:text-blue-400 dark:hover:border-blue-400 dark:focus:border-blue-400"
+                >
+                  Continue Reading
+                  <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            <div className="overflow-hidden bg-white rounded shadow dark:bg-gray-700">
+              <div className="p-5">
+                <div className="relative">
+                  <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                    <img
+                      className="object-cover w-full h-full"
+                      src="https://cdn.rareblocks.xyz/collection/celebration/images/blog/2/blog-post-2.jpg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="absolute top-4 left-4">
+                    <span className="px-4 py-2 text-xs font-semibold tracking-widest text-gray-900 uppercase bg-white rounded-full dark:text-gray-100 dark:bg-gray-800">
+                      Design
+                    </span>
+                  </div>
+                </div>
+                <span className="block mt-6 text-sm font-semibold tracking-widest text-gray-500 uppercase dark:text-gray-400">
+                  April 04, 2020
+                </span>
+                <p className="mt-5 text-2xl font-semibold">
+                  <a href="#" title="" className="text-black dark:text-white">
+                    A guide to latest design trend for 2020.
+                  </a>
+                </p>
+                <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
+                  enim velit mollit.
+                </p>
+                <a
+                  href="#"
+                  title=""
+                  className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600 dark:text-blue-400 dark:hover:border-blue-400 dark:focus:border-blue-400"
+                >
+                  Continue Reading
+                  <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            <div className="overflow-hidden bg-white rounded shadow dark:bg-gray-700">
+              <div className="p-5">
+                <div className="relative">
+                  <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                    <img
+                      className="object-cover w-full h-full"
+                      src="https://cdn.rareblocks.xyz/collection/celebration/images/blog/2/blog-post-3.jpg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="absolute top-4 left-4">
+                    <span className="px-4 py-2 text-xs font-semibold tracking-widest text-gray-900 uppercase bg-white rounded-full dark:text-gray-100 dark:bg-gray-800">
+                      Productivity
+                    </span>
+                  </div>
+                </div>
+                <span className="block mt-6 text-sm font-semibold tracking-widest text-gray-500 uppercase dark:text-gray-400">
+                  May 12, 2020
+                </span>
+                <p className="mt-5 text-2xl font-semibold">
+                  <a href="#" title="" className="text-black dark:text-white">
+                    5 Productivity tips to write faster at morning.
+                  </a>
+                </p>
+                <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
+                  enim velit mollit.
+                </p>
+                <a
+                  href="#"
+                  title=""
+                  className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600 dark:text-blue-400 dark:hover:border-blue-400 dark:focus:border-blue-400"
+                >
+                  Continue Reading
+                  <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center mt-8 space-x-3 lg:hidden">
+            <button
+              type="button"
+              className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white dark:border-gray-600 dark:hover:bg-blue-500 dark:focus:bg-blue-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+              </svg>
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white dark:border-gray-600 dark:hover:bg-blue-500 dark:focus:bg-blue-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
