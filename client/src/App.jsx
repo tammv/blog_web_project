02f5867dd3a2f8b45123cd9b8bd40e681c6efdca
Tammv <72090950+tammv@users.noreply.github.com>
2024-06-03 17:37:28 +0700

    post UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,23 +7,25 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CreatePost from "./pages/CreatePost";
 import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route element={<PrivateRoute/>}>
-          <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
-        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
