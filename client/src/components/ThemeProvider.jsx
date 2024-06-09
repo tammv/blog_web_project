137c0189fcc688e7af8e4619701e1d335d47e9c4
Tammv <72090950+tammv@users.noreply.github.com>
2024-06-09 17:18:16 +0700

    sidebar fix

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -4,9 +4,5 @@ export default function ThemeProvider({ children }) {
   // Destructure children from props
   const { theme } = useSelector((state) => state.theme);
 
-  return (
-    <div className={`${theme}bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen`}>
-      {children}
-    </div>
-  );
+  return <div className={`${theme} min-h-screen`}>{children}</div>;
 }
