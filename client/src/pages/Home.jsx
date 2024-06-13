f75fd7a7ab5a6e5f55cd81a4fd8f5710c298870b
Tammv <72090950+tammv@users.noreply.github.com>
2024-06-13 22:05:14 +0700

    update home 1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <div>
-        {currentUser.isAdmin && (
+        {currentUser && currentUser.isAdmin && (
           <section className="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24 dark:bg-gray-900">
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
               <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
